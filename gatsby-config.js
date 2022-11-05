4ad3062466789dbd0d807ff4215fd322810818b6
anastasiakounoupi <anastasia.kunupi@gmail.com>
2022-11-05 18:26:30 +0200

    add site metadata

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://www.mydomain.com',
+    title: 'Frontend Masters Gatsby',
+    description: 'Frontend Masters intro to Gatsby project',
+    image: 'https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
+  },
+};
