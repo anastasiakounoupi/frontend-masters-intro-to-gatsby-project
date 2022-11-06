021b0cd1fdb340b6cb3056d00f67ea1bd00581a4
anastasiakounoupi <anastasia.kunupi@gmail.com>
2022-11-06 20:08:26 +0200

    add metadata and seo component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
+import { Seo } from '../components/seo.js';
 
 export default function AboutPage() {
   return (
-    <main>
-      <h1>About Page</h1>
-      <Link to="/">Back to Home</Link>
-    </main>
+    <>
+      <Seo
+        title="About this Site"
+        description="More information about this site."
+      />
+      <main>
+        <h1>About Page</h1>
+        <Link to="/">Back to Home</Link>
+      </main>
+    </>
   )
 }
